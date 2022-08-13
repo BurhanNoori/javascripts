b5dad67fb92d74018f76605667e79233a435a9bb
BurhanNoori <burhan.batawala78@gmail.com>
2022-08-13 21:19:02 +0530

    Day 2 Commit

--- OnlineCalculator/calculator.js
+++ OnlineCalculator/calculator.js
@@ -1,62 +1,65 @@
-let input = document.getElementById('inputScreen'),
-    buttons = document.querySelectorAll('button'),
+const input = document.getElementById('inputScreen'),
+    numbers = document.querySelectorAll('.number'),
+    operators = document.querySelectorAll('.operator');
     output = document.getElementById('outputScreen');
  
 
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        
-        let buttonText = e.target.innerText;
-
-        switch (buttonText) {
-            case 'BCKSP':
-                input.value = input.value.substring(0, input.value.length - 1);
-                break;
-            case 'sqrt':
-                output.value = Math.sqrt(+input.value);
-                break;
-            case 'sin':
-            case 'cos':
-            case 'tan':
-            case 'ln':
-            case 'log':
-            case 'mod':
-            case 'pi':
-            case 'P':
-            case 'C':
-            case '=':
-            case '+':
-                output.value="";
-                input.value += buttonText;
-                input.value.split("+")
-                break;
-            case '-':
-                input.value += buttonText;
-                break;
-            case '*':
-                input.value += buttonText;
-                break;
-            case '/':
-                input.value += buttonText;
-                break;
-            case '%':
-                input.value += buttonText;
-                break;
-            case '^':
-            case '.':
-            case 'e':
-            case 'e^x':
-            case 'deg':
-            case 'rad':
-            case '1/x':
-            case '(':
-            case ')':
-            case '!':
-            break;
-            default: 
-            input.value += buttonText;
-            //console.log(inputValue);
-        }
-    });
-
+numbers.forEach(number=>{
+    number.addEventListener('click', (e)=>{
+        input.value+=e.target.innerText;
+        const value = input.value;
+        Array.from(value).forEach(char=>{
+            console.log('char ', char);
+            if (char==='+' || char ==='-' || char === '*' || char === '/' || char === 'm'){
+                const operatorIndex = value.indexOf(char);
+                const number1 =  +value.slice(0,operatorIndex);
+                const number2 = +value.slice(operatorIndex + 1);
+                const operator = char;
+                output.value = compute(number1, number2, operator);
+                return
+                
+            }
+        })
+    })
 })
+
+operators.forEach(operator=>{
+    operator.addEventListener('click', (e)=>{
+        if(output.value) input.value = output.value;
+
+
+        lastChar = input.value.slice(-1);
+        
+        if(lastChar===e.target.innerText) return;
+        if(isNaN(+lastChar)) {
+            
+            input.value=input.value.replace(lastChar, operator.innerText);
+            return;
+        }
+   
+        input.value+=e.target.innerText;
+
+
+
+    });
+})
+
+
+const compute = function(number1, number2, operator){
+    if(number1===undefined || number2===undefined || operator===undefined) return;
+    switch (operator) {
+
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            return number1 / number2;
+        case 'm':
+            return number1 % number2;
+        default:
+            return
+    }
+}
